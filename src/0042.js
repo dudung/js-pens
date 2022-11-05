3883a4d962aa0c1f9f536e42555d8ce107b2cc53
dudung <dudung@gmail.com>
2022-11-05 21:05:18 +0700

    0042, beg, pause

---
+++ src/0042.js
@@ -0,0 +1,16 @@
+/*
+  0042.js
+  Use div element for preformated but with color
+  
+  Sparisoma Viridi | https://github.com/dudung/js-pens
+  
+  20221105
+  2104 Start this code with empty but this information.
+*/
+
+
+main();
+
+
+function main() {
+}
