3a1e8eedc456b77ba2cd1ddf150a0093e514ebc5
Sparisoma Viridi <dudung@gmail.com>
2022-11-05 09:24:46 +0700

    0038, new

---
+++ src/0038.js
@@ -0,0 +1,13 @@
+/*
+	0038.js
+	Some elements of HTML
+	
+	Sparisoma Viridi | https://github.com/dudung/js-pens
+	
+  20221105
+  0923 Fin info about the elements of HTML [1].
+	
+	References
+	1. url https://html.spec.whatwg.org/#toc-semantics
+*/
+
